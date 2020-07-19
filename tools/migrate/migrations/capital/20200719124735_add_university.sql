2bd2cab0382e44ea3a34cff381c7047a9cc0f614
VashchilkoAV <vashchilko.av@phystech.edu>
2020-07-19 13:17:03 +0300

    [issue-4]: sql query

---
+++ tools/migrate/migrations/capital/20200719124735_add_university.sql
@@ -0,0 +1,25 @@
+-- +goose Up
+create table university
+(
+    id serial not null,
+    name varchar not null,
+    on_main_page boolean not null,
+    in_filter boolean not null,
+    added_at timestamp not null,
+    updated_at timestamp not null,
+    position int not null,
+    img varchar not null
+);
+
+create unique index univesity_id_uindex
+    on university (id);
+
+create unique index university_name_uindex
+    on univestity (name);
+
+alter table university
+    add constraint university_pk
+        primary key (id);
+
+-- +goose Down
+drop table university if exists;
