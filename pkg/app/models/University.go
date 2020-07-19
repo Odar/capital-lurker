593d0fb123af2ee1308722c41b9159252fc514e3
VashchilkoAV <vashchilko.av@phystech.edu>
2020-07-19 13:55:47 +0300

    [issue-4]: University.go is fixed

--- pkg/app/models/University.go
+++ pkg/app/models/University.go
@@ -1,1 +1,14 @@
 package models
+
+import "time"
+
+type University struct {
+	ID         uint64    `db:"id"`
+	Name       string    `db:"name"`
+	OnMainPage bool      `db:"on_main_page"`
+	InFilter   bool      `db:"in_filter"`
+	AddedAt    time.Time `db:"added_at"`
+	UpdatedAt  time.Time `db:"updated_at"`
+	Position   uint64    `db:"position"`
+	Img        string    `db:"img"`
+}
