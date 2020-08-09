60874a596bac0e505aeb63857d82d28413baf0b5
VashchilkoAV <vashchilko.av@phystech.edu>
2020-08-10 02:54:23 +0300

    [oauth2]: working, todo: 1) visualisation

--- internal/authentification/authentificator.go
+++ internal/authentification/authentificator.go
@@ -1,14 +1,13 @@
 package authentification
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
-	"os"
-
+	vk "github.com/go-vk-api/vk"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/vk"
-	"github.com/urShadow/go-vk-api"
+	vkAuth "golang.org/x/oauth2/vk"
+	"net/http"
 )
 
 type authentificator struct {
@@ -20,11 +19,11 @@ func New() *authentificator {
 
 var (
 	vkOauthConfig = &oauth2.Config{
-		ClientID:     os.Getenv("CLIENT_ID"),
-		ClientSecret: os.Getenv("CLIENT_SECRET"),
-		RedirectURL:  os.Getenv("REDIRECT_URL"),
+		ClientID:     "7562746",                    //os.Getenv("CLIENT_ID"),
+		ClientSecret: "rqozJXDcBPrygXS181xr",       //os.Getenv("CLIENT_SECRET"),
+		RedirectURL:  "http://localhost:8888/home", //os.Getenv("REDIRECT_URL"),
 		Scopes:       []string{},
-		Endpoint:     vk.Endpoint,
+		Endpoint:     vkAuth.Endpoint,
 	}
 	randomState = "haha"
 )
@@ -35,14 +34,42 @@ func (a *authentificator) GetInfoFromVK(ctx echo.Context) error {
 	}
 	token, err := vkOauthConfig.Exchange(oauth2.NoContext, ctx.FormValue("code"))
 	if err != nil {
-		return ctx.String(http.StatusInternalServerError, fmt.Sprintf("could not get token %s\n", err.Error()))
+		return ctx.String(http.StatusInternalServerError, fmt.Sprintf("can not get token %s\n", err.Error()))
 	}
 
-	client, err := go_vk_api.
+	client, err := vk.NewClientWithOptions(vk.WithToken(token.AccessToken))
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, fmt.Sprintf("can not create vk api client %s\n", err.Error()))
+	}
+
+	user := getCurrentUser(client)
+	return json.NewEncoder(ctx.Response()).Encode(user)
+}
+
+func getCurrentUser(api *vk.Client) User {
+	var users []User
+
+	api.CallMethod("users.get", vk.RequestParams{
+		"fields": "photo_400_orig,city",
+	}, &users)
+
+	return users[0]
+}
+
+type User struct {
+	ID        int64  `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Photo     string `json:"photo_400_orig"`
+	City      City   `json:"city"`
+}
+
+type City struct {
+	Title string `json:"title"`
 }
 
 func (a *authentificator) Login(ctx echo.Context) error {
 	url := vkOauthConfig.AuthCodeURL(randomState)
-	ctx.Redirect(http.StatusOK, url)
-	return nil
+	err := ctx.Redirect(http.StatusTemporaryRedirect, url)
+	return err
 }
--- internal/server/routing.go
+++ internal/server/routing.go
@@ -11,6 +11,6 @@ func (s *server) setRoutes() {
 	s.echo.POST("/admin/university/:id", s.universityAdminer.UpdateUniversity)
 	s.echo.File("/login", "assets/loginPage.html")
 	s.echo.Any("/auth", s.authentificator.Login)
-	s.echo.File("/home", "assets/homePage.html")
-	s.echo.Any("/home", s.authentificator.GetInfoFromVK)
+	//s.echo.File("/home", "assets/homePage.html")
+	s.echo.GET("/home", s.authentificator.GetInfoFromVK)
 }
