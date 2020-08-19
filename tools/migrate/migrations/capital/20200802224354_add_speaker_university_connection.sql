d067d515fe025d2976a7f11b9307f18b321ed493
MuXauJl11110 <persiianov.mi@phystech.edu>
2020-08-19 16:20:37 +0300

    [issue-25]: Added fk constraint

--- tools/migrate/migrations/capital/20200802224354_add_speaker_university_connection.sql
+++ tools/migrate/migrations/capital/20200802224354_add_speaker_university_connection.sql
@@ -1,3 +1,9 @@
 -- +goose Up
 alter table speaker
 add column university_id bigint default null;
+
+alter table speaker
+    add constraint speaker_university_fk
+        foreign key (university_id)
+            references university (id)
+            on delete set null;
